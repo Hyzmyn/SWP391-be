862b3912aace1497450aa6237afd265bcde5274c
Pham Duc Tai <149134916+dtai-coding@users.noreply.github.com>
2024-09-22 09:52:12 +0700

    Initial

---
+++ ModelLayer/Entities/AdoptionRegistrationForm.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelLayer.Entities
+{
+    public class AdoptionRegistrationForm : BaseEntity
+    {
+        public string IdentityProof { get; set; }
+        public decimal IncomeAmount { get; set; }
+        public string Image { get; set; }
+        public string Condition { get; set; }
+        public int AdopterId { get; set; }
+        public int ShelterStaffId { get; set; }
+        public int PetId { get; set; }
+
+        public virtual User? User { get; set; }
+        public virtual Pet? Pet { get; set; }
+
+    }
+}
---
+++ ModelLayer/Entities/FeedBack.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelLayer.Entities
+{
+    public class FeedBack : BaseEntity
+    {
+        public string UserId { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+
+        public virtual Post? Post { get; set; }
+    }
+}
