edae311df09554efe28e55fa4def8137d9ab866f
Pham Duc Tai <149134916+dtai-coding@users.noreply.github.com>
2024-09-26 10:02:57 +0700

    fix jwt

--- RepositoryLayer/obj/Debug/net8.0/RepositoryLayer.AssemblyInfo.cs
+++ RepositoryLayer/obj/Debug/net8.0/RepositoryLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RepositoryLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afea516d8ff2eae3b59404ee1e640d3e28f0ff43")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe830c119364758c7798a0c0219d360623ee1178")]
 [assembly: System.Reflection.AssemblyProductAttribute("RepositoryLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RepositoryLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
