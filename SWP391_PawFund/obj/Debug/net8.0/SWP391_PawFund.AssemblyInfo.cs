4e2cfa2d39d099d0f0c73a83992d4e4dab76c55f
Pham Duc Tai <149134916+dtai-coding@users.noreply.github.com>
2024-09-28 21:22:52 +0700

    seed data, fix db

--- SWP391_PawFund/obj/Debug/net8.0/SWP391_PawFund.AssemblyInfo.cs
+++ SWP391_PawFund/obj/Debug/net8.0/SWP391_PawFund.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SWP391_PawFund")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d15e7f5ffb8d7869c676cd5ab4a73f976083a793")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+647a835a2a991bdb579dc514739275fdafc5c396")]
 [assembly: System.Reflection.AssemblyProductAttribute("SWP391_PawFund")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SWP391_PawFund")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
