300b54d4abc9d9e000e04ebb2ea5e34d158ac73d
khiemvuong2310 <nguyenvuongvankhiem1132@gmail.com>
2024-09-29 13:11:43 +0700

    Create StatusPet Request , Response Model

---
+++ ServiceLayer/ResponseModels/BaseEntityResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLayer.ResponseModels
+{
+    public class BaseEntityResponse
+    {
+        public bool? Status { get; set; }
+    }
+}
---
+++ ServiceLayer/ResponseModels/StatusResponseModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceLayer.ResponseModels
+{
+    public class StatusResponseModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Date { get; set; } = string.Empty; // Nếu có thể, nên sử dụng DateTime thay vì string
+        public string Disease { get; set; } = string.Empty;
+        public string Vaccine { get; set; } = string.Empty;
+        public int PetId { get; set; }
+        public string PetName { get; set; } = string.Empty;
+    }
+    public class StatusDetailResponseModel : StatusResponseModel
+    {
+        public PetResponseModel Pet { get; set; } = null!;
+    }
+
+    public class PetDetailResponseModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        // Thêm các thuộc tính khác của Pet nếu cần
+    }
+
+}
