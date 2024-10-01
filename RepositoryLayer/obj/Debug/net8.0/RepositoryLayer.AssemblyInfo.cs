c782263f641f203d90e6b1dd918d9f6d95f05522
khiemvuong2310 <nguyenvuongvankhiem1132@gmail.com>
2024-10-01 08:40:16 +0700

    Create CRUD for Certification , add Service for Post , Request and Response Post + FeedBack

--- RepositoryLayer/obj/Debug/net8.0/RepositoryLayer.AssemblyInfo.cs
+++ RepositoryLayer/obj/Debug/net8.0/RepositoryLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RepositoryLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+daad344493f6c6ae24122ba1db457e61be92a372")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7451b54898416e3979087ae6de7545a19d0af5db")]
 [assembly: System.Reflection.AssemblyProductAttribute("RepositoryLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RepositoryLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ServiceLayer/Services/FeedbackService.cs
+++ ServiceLayer/Services/FeedbackService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ModelLayer.Entities;
+using RepositoryLayer.UnitOfWork;
+using ServiceLayer.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace ServiceLayer.Services
 {
-    internal class FeedbackService
+    public class FeedbackService
     {
     }
 }
