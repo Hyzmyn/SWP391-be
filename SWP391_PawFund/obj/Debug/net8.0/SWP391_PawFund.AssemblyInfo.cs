5c5b3b05e4da1e152342fe1f4dccdea7260c226f
HoDinhBinh <hodinhbinh2503@gmail.com>
2024-10-15 13:00:05 +0700

    feat: basic config for vn pay | 10152024

--- SWP391_PawFund/obj/Debug/net8.0/SWP391_PawFund.AssemblyInfo.cs
+++ SWP391_PawFund/obj/Debug/net8.0/SWP391_PawFund.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SWP391_PawFund")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00da0f3082a7e7e117a1b1f899aa6bff61b71719")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7d44fa9970003c306a532073078d1aea1926c9d")]
 [assembly: System.Reflection.AssemblyProductAttribute("SWP391_PawFund")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SWP391_PawFund")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
