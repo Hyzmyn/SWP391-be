59a248679921f15520b6ea7274d8c0e6097d61a2
khiemvuong2310 <nguyenvuongvankhiem1132@gmail.com>
2024-10-15 13:14:36 +0700

    Tạo lại CRUD cho Pet trả về List PetStaus và cả Status . Thêm 1 số function CRUD cho Pet gán Status

--- ModelLayer/obj/Debug/net8.0/ModelLayer.AssemblyInfo.cs
+++ ModelLayer/obj/Debug/net8.0/ModelLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ModelLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+487b9d228989047d56964682e93d0f5a348ca1f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6842f3342cd360374f370deabba7edbf63a9d008")]
 [assembly: System.Reflection.AssemblyProductAttribute("ModelLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ModelLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
