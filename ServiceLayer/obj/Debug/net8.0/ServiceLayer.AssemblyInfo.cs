cb34c379be632003069660f77cd85e9500acf98e
Pham Duc Tai <149134916+dtai-coding@users.noreply.github.com>
2024-10-21 09:43:11 +0700

    a

--- ServiceLayer/obj/Debug/net8.0/ServiceLayer.AssemblyInfo.cs
+++ ServiceLayer/obj/Debug/net8.0/ServiceLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServiceLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+783422956adf0ec3e34192ce7795705376f529c5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95823e1ae0bdc97a18e1b364e4f0a7bd2c8e7b2e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServiceLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServiceLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
