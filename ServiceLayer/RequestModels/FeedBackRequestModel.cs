7e0a3cb631e24107cd8cdb2e461c38a5f6e343e8
khiemvuong2310 <nguyenvuongvankhiem1132@gmail.com>
2024-10-28 23:14:47 +0700

    Chỉnh sửa lại FeedBack

--- ServiceLayer/RequestModels/FeedBackRequestModel.cs
+++ ServiceLayer/RequestModels/FeedBackRequestModel.cs
@@ -16,7 +16,7 @@ namespace ServiceLayer.RequestModels
         [StringLength(1000, ErrorMessage = "Description cannot exceed 1000 characters.")]
         public string Description { get; set; }
 
-        [Required]
-        public int PostId { get; set; }
+        //[Required]
+        public int? PostId { get; set; }
     }
 }
